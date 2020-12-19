3915aa0159cc6b52b2549a7d5d9f53bc0f154a9d
rushiwadkar007 <rwadkar920@gmail.com>
2020-12-19 23:37:18 +0530

    app created

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const http = require('http').createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
+
+
 http.listen(PORT, () =>{
     console.log(`Listening on PORT ${PORT}`);
 });
@@ -14,3 +16,14 @@ app.use(express.static(__dirname+'/public'))
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
+
+//socket.
+
+const io = require('socket.io')(http);
+
+io.on('connection', (socket)=>{
+    console.log('Connected...');
+    socket.on('message', (msg)=>{
+        socket.broadcast.emit('message', msg)
+    })
+})
